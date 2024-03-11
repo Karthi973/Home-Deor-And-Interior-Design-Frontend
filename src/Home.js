1d97cd2a7c1ad8dc415fbf0a89277cc7ee440815
karthi973 <karthikadevi978@gmail.com>
2024-03-11 04:10:46 -0700

    first commit

---
+++ src/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className="home-container">
+      <h3>Welcome to React-App</h3>
+      <img className="home-img" src="https://cdn-bnokp.nitrocdn.com/QNoeDwCprhACHQcnEmHgXDhDpbEOlRHH/assets/images/optimized/rev-8fd3595/www.decorilla.com/online-decorating/wp-content/uploads/2021/09/Luxury-contemporary-interior-design-style-by-Decorilla-designer-Nathalie-I.jpeg" alt="Welcome picture" />
+    </div>
+  )
+}
---
+++ src/global.js
@@ -0,0 +1,1 @@
+export const API = "https:home-decor-and-interior-designs.vercel.app"
